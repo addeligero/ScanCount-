fb6d4eec1d6f7110dedc60e36cb0cc28fa2f6b86
ADRIAN <adriandeligero.1030@gmail.com>
2025-04-12 11:23:43 +0800

    styles: added image and forms in welcome page

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -7,7 +7,21 @@ const theme = useTheme()
 function onClick() {
   theme.global.name.value = theme.global.name.value === 'light' ? 'dark' : 'light'
 }
-const text = ref('')
+const firstName = ref('')
+const firstNameRules = [
+  (value) => {
+    if (value?.length >= 3) return true
+    return 'First name must be at least 3 characters.'
+  },
+]
+
+const lastName = ref('123')
+const lastNameRules = [
+  (value) => {
+    if (/[^0-9]/.test(value)) return true
+    return 'Last name can not contain digits.'
+  },
+]
 </script>
 
 <template>
@@ -27,6 +41,7 @@ const text = ref('')
 
     <v-main>
       <v-container fluid class="fill-height d-flex align-center justify-center">
+        <img src="@/assets/img/1.png" alt="" style="max-width: 50%" />
         <v-sheet class="mx-auto" width="300">
           <v-form fast-fail @submit.prevent>
             <v-text-field
