1f33071381d09e59ee3b6326df51b6a08b412e81
ADRIAN <adriandeligero.1030@gmail.com>
2025-04-14 19:54:11 +0800

    add: separate page for register

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/Welcome.vue'
+import Register from '@/views/auth/Register.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Welcome,
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
     },
   ],
 })
