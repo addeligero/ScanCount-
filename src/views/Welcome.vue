1541764f8e59f14a086f5b8fa62026d26d0ca925
ADRIAN <adriandeligero.1030@gmail.com>
2025-04-14 19:31:16 +0800

    add: separated file for auth-login

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,27 +1,12 @@
 <script setup>
-import { ref } from 'vue'
 import { useTheme } from 'vuetify'
+import Login from '@/components/Auth/Login.vue'
 
 const theme = useTheme()
 
 function onClick() {
   theme.global.name.value = theme.global.name.value === 'light' ? 'dark' : 'light'
 }
-const firstName = ref('')
-const firstNameRules = [
-  (value) => {
-    if (value?.length >= 3) return true
-    return 'First name must be at least 3 characters.'
-  },
-]
-
-const lastName = ref('123')
-const lastNameRules = [
-  (value) => {
-    if (/[^0-9]/.test(value)) return true
-    return 'Last name can not contain digits.'
-  },
-]
 </script>
 
 <template>
@@ -42,23 +27,7 @@ const lastNameRules = [
     <v-main>
       <v-container fluid class="fill-height d-flex align-center justify-center">
         <img src="@/assets/img/1.png" alt="" style="max-width: 50%" />
-        <v-sheet class="mx-auto" width="300">
-          <v-form fast-fail @submit.prevent>
-            <v-text-field
-              v-model="firstName"
-              :rules="firstNameRules"
-              label="First name"
-            ></v-text-field>
-
-            <v-text-field
-              v-model="lastName"
-              :rules="lastNameRules"
-              label="Last name"
-            ></v-text-field>
-
-            <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-          </v-form>
-        </v-sheet>
+        <Login />
       </v-container>
     </v-main>
   </v-app>
