f358efb709541adf51f0e0020c21955b17f8a39b
ADRIAN <adriandeligero.1030@gmail.com>
2025-04-19 11:12:49 +0800

    add: installed dependencies for icons and implemented it

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,8 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import '@mdi/font/css/materialdesignicons.css'
+
 const savedTheme = localStorage.getItem('theme') || 'light'
 const vuetify = createVuetify({
   components,
