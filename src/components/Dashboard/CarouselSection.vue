1a6b5b866b0b87eacd4fdcc2791ad025eed73797
ADRIAN <adriandeligero.1030@gmail.com>
2025-05-01 16:41:00 +0800

    fixed and hide delimeters

--- src/components/Dashboard/CarouselSection.vue
+++ src/components/Dashboard/CarouselSection.vue
@@ -7,7 +7,7 @@ const images = [image1, image2, image3]
 </script>
 
 <template>
-  <v-carousel height="400" show-arrows="hover" cycle hide-delimiter-background>
+  <v-carousel height="400" show-arrows="hover" cycle hide-delimiters>
     <v-carousel-item v-for="(img, i) in images" :key="i">
       <v-img :src="img" height="100%" contain> </v-img>
     </v-carousel-item>
